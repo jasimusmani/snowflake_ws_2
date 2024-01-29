5ebdc811acf7513f66ac4c2c51e8a59332d9f727
Jasim Usmani <46856524+jasimusmani@users.noreply.github.com>
2024-01-29 09:45:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,7 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
+
+# Let's put a pick list here so they can pick the fruit they want to include
+streamlit.multiselect("Pick some fruits:", list(my fruit list.index))
 streamlit.dataframe(my_fruit_list)
